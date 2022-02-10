b7931806b2566e5b2d9d19811bd39e8f353a0900
Eugene Serb <46799701+eugene-serb@users.noreply.github.com>
2022-02-10 21:44:58 +0300

    made marking and saving "checked" and "important"

--- js/todolist.js
+++ js/todolist.js
@@ -1,41 +1,74 @@
 window.onload = function () {
 
-    var todoList = [];
+    let todoList = [];
+
     if (localStorage.getItem('todo') != undefined) {
         todoList = JSON.parse(localStorage.getItem('todo'));
-        out();
+        displayTasks();
     }
 
-    document.getElementById('add').onclick = function () {
-        var d = document.getElementById('in').value;
-
-        var temp = {};
-        temp.todo = d;
-        temp.check = false;
-
-        var i = todoList.length;
-        todoList[i] = temp;
-        console.log(todoList);
-
-        out();
-        localStorage.setItem('todo', JSON.stringify(todoList));
-    };
-
-    function out() {
-        var out = '';
+    function displayTasks() {
+        
+        let out = '';
 
         for (var key in todoList) {
-
-            if (todoList[key].check === true) {
-                out += '<input type="checkbox" checked />';
-            } else {
-                out += '<input type="checkbox" />';
-            }
-
-            out += todoList[key].todo + '<br />';
+            out += `
+                <li ${todoList[key].important ? 'class="important"' : '' }>
+                    <input id="item_${key}" type="checkbox" ${todoList[key].check ? "checked" : ""} />
+                    <label for="item_${key}">${todoList[key].todo}</label>
+                </li>`;
         };
 
-        document.getElementById('out').innerHTML = out;
+        document.querySelector('#desk').innerHTML = out;
+    };
+
+    document.querySelector('#desk').addEventListener('change', () => {
+        let idInput = event.target.getAttribute('id');
+        let forLabel = document.querySelector('#desk').querySelector('[for=' + idInput + ']');
+        let valueLabel = forLabel.innerHTML;
+
+        todoList.forEach(function(item) {
+            if (item.todo === valueLabel) {
+                item.check = !item.check;
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        });
+    });
+
+    document.querySelector('#desk').addEventListener('contextmenu', function (event) {
+
+        event.preventDefault();
+
+        todoList.forEach(function (item, index) {
+            if (item.todo === event.target.innerHTML) {
+                if (event.ctrlKey || event.metaKey) {
+                    todoList.splice(index, 1);
+                } else {
+                    item.important = !item.important;
+                }
+                displayTasks();
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        });
+    });
+
+    document.querySelector('#addButton').onclick = function () {
+
+        const message = document.querySelector('#message');
+        if (!message.value) return;
+
+        let newTask = {
+            todo: message.value,
+            check: false,
+            important: false
+        };
+
+        todoList.push(newTask);
+
+        displayTasks();
+        message.value = '';
+
+        localStorage.setItem('todo', JSON.stringify(todoList));
     };
 };
 
